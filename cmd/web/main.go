9e7f313c6bdbfe4f18f10fcb476f1bde4e461942
Daniel Tinivella <dtinivella@gmail.com>
2022-12-29 19:08:03 -0300

    add template Home and navbar

---
+++ cmd/web/main.go
@@ -0,0 +1,24 @@
+//
+// main.go
+// Copyright (C) 2022 tinix <tinix@archlinux>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
